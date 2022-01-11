914ba2b4de0ccad7a64f38510fa367ce0a52e3e3
GBocharof <i10892i@gmail.com>
2022-01-11 15:00:06 +0300

    main

---
+++ studyChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  studyChat
+//
+//  Created by Gleb Bocharov on 11.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
